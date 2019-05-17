e229d208efdf1cb24d4eca74c05e12aec359dab2
Miquel Vicens <mvicens@proadata.com>
2019-05-17 09:57:12 +0200

    Fix compiling bug
    Also hoisted function.

--- src/scripts/factories.js
+++ src/scripts/factories.js
@@ -8,13 +8,17 @@ function getAntiloopDirectiveCompileOption( $compile ) {
 	return function( domTransformation, scopeLinking ) {
 		return function( tElement, tAttrs ) {
 			domTransformation( tElement, tAttrs );
-			return function( scope, iElement, iAttrs ) {
+			return {
+				pre: preLink
+			};
+
+			function preLink( scope, iElement, iAttrs ) {
 				if ( scopeLinking )
 					scopeLinking( scope, iElement, iAttrs );
 
 				iElement.removeAttr( iAttrs.$attr[ this.name ] );
 				$compile( iElement )( scope );
-			};
+			}
 		};
 	};
 }
